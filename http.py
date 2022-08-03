2d4db61cf868fe51ed3a76e577b075c7dc8b3046
Alexandr_Popov <Reorin262@gmail.com>
2022-08-03 17:12:44 +0300

    Не удается импортировать requests

---
+++ http.py
@@ -0,0 +1,45 @@
+from ast import Name
+from pprint import pprint
+import json
+import requests
+
+url = 'akabab.github.io/superhero-api/api'
+
+content_Hulk = json.loads(requests.get('https://www.superheroapi.com/api.php/2619421814940190/search/Hulk').content)
+intel_Hulk = int(content_Hulk['results'][0]['powerstats']['intelligence'])
+content_Captain = json.loads(requests.get('https://www.superheroapi.com/api.php/2619421814940190/search/Captain').content)
+intel_Captain = int(content_Captain['results'][0]['powerstats']['intelligence'])
+content_Thanos = json.loads(requests.get('https://www.superheroapi.com/api.php/2619421814940190/search/Thanos').content)
+intel_Thanos = int(content_Thanos['results'][0]['powerstats']['intelligence'])
+intel_dict = {'Hulk': intel_Hulk, 'Captain america': intel_Captain, 'Thanos': intel_Thanos}
+
+print({max(intel_dict)})
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers (self):
+        return {"Authorization": 'OAuth {}'.format(self.token)}
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        header = self.get_headers()
+        = = {'path': disk_file_path, 'overwrite': 'true'}
+        response = requests.get(upload_url, headers=header, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload (self, file_path: str):
+        href = self._get_upload_link(disk_file_path=d_file_path).get('href')
+        response = requests.put(href, data=open(file_path, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print ('Success')
+
+
+if Name == '__main__':
+    TOKEN = None
+    d_file_path = 'important_file.txt'
+    uploader = YaUploader (token=TOKEN)
+    uploader.upload('important_file.txt')
